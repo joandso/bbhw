6b121dce4ffc30099bbc8bb0c808f4fb41092367
joandso <95076396+joandso@users.noreply.github.com>
2024-08-16 12:01:40 +0100

    Update towns.js
    zoom cluster edit

--- towns.js
+++ towns.js
@@ -86,7 +86,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     features: window.hotelFeatures
                 },
                 cluster: true,
-                clusterMaxZoom: 15,
+                clusterMaxZoom: 5,
                 clusterRadius: 10
             });
 
